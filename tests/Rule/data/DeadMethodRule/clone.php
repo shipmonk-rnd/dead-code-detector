034c8bbfd39f047df9e5e46164787d5417a9dfa7
Jan Nedbal <janedbal@gmail.com>
2024-09-12 17:17:56 +0200

    Test also clone call via parent::__clone

--- tests/Rule/data/DeadMethodRule/clone.php
+++ tests/Rule/data/DeadMethodRule/clone.php
@@ -6,12 +6,18 @@ class CloneClass1 {
     public function __clone() {}
 }
 
+class CloneClass2 {
+    public function __clone() {} // error: Unused DeadClone\CloneClass2::__clone
+}
+
 class ParentToClone {
-    public function __clone() {} // error: Unused DeadClone\ParentToClone::__clone
+    public function __clone() {}
 }
 
 class ChildClass extends ParentToClone {
-    public function __clone() {}
+    public function __clone() {
+        parent::__clone();
+    }
 }
 
 clone new CloneClass1();
