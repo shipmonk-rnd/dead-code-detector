458ee70e7bcd440259f6a2bef6fe50bb55080114
Jan Nedbal <janedbal@gmail.com>
2024-09-14 14:02:52 +0200

    Doctrine: support EntityRepository constructor

--- tests/Rule/data/DeadMethodRule/providers/doctrine.php
+++ tests/Rule/data/DeadMethodRule/providers/doctrine.php
@@ -2,6 +2,10 @@
 
 namespace Doctrine;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadata;
+
 class MyEntity
 {
 
@@ -17,6 +21,17 @@ class MyListener {
     public function afterUpdate(): void {}
 
     public function postPersist(): void {}
+
+}
+
+class FooRepository extends EntityRepository {
+
+    public function __construct(
+        EntityManagerInterface $em,
+        ClassMetadata $class
+    ) {
+        parent::__construct($em, $class);
+    }
 
 }
 
