a9de4d2e2bb653d75c62e3daa0d842fe145cd84b
Jan Nedbal <janedbal@gmail.com>
2025-01-24 15:27:23 +0100

    Fix detection of excluded mixed usages

--- src/Graph/ClassMethodUsage.php
+++ src/Graph/ClassMethodUsage.php
@@ -2,6 +2,7 @@
 
 namespace ShipMonk\PHPStan\DeadCode\Graph;
 
+use LogicException;
 use ShipMonk\PHPStan\DeadCode\Enum\MemberType;
 
 /**
@@ -37,6 +38,22 @@ final class ClassMethodUsage extends ClassMemberUsage
     public function getMemberRef(): ClassMethodRef
     {
         return $this->callee;
+    }
+
+    public function concretizeMixedUsage(string $className): self
+    {
+        if ($this->callee->getClassName() !== null) {
+            throw new LogicException('Usage is not mixed, thus it cannot be concretized');
+        }
+
+        return new self(
+            $this->getOrigin(),
+            new ClassMethodRef(
+                $className,
+                $this->callee->getMemberName(),
+                $this->callee->isPossibleDescendant(),
+            ),
+        );
     }
 
 }
