57f589c7576d5b1c2038c0e3d08004657d9ac083
Jan Nedbal <janedbal@gmail.com>
2025-03-25 15:16:33 +0100

    Improve descendant detection for const fetches (#176)

--- tests/Rule/data/constants/descendant-4.php
+++ tests/Rule/data/constants/descendant-4.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace DeadConstStatic;
+namespace DeadConstDescendant4;
 
 
 class P {
--- tests/Rule/data/constants/traits-14.php
+++ tests/Rule/data/constants/traits-14.php
@@ -3,16 +3,20 @@
 namespace DeadTraitConst14;
 
 trait SomeTrait {
-    const FOO = 1; // error: Unused DeadTraitConst14\SomeTrait::FOO
+    const FOO = 1;
 }
 
 class ParentClass {
-    const FOO = 1;
+    const FOO = 1; // error: Unused DeadTraitConst14\ParentClass::FOO
 }
 
 class User extends ParentClass {
     use SomeTrait;
 }
 
-echo User::FOO; // verify by var_dump((new ReflectionClass('User'))->getReflectionConstants());
+echo User::FOO;
+
+// this test does not fully comply with result of var_dump((new ReflectionClass('User'))->getReflectionConstants());
+// this behaviour is kept for simplicity as it has equal behaviour with methods (see methods/traits-14.php)
+// also, overridden constants are ensured to have the same value
 
