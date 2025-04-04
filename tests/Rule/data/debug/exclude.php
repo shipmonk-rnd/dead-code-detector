337b299d0e56d0727f2dd537fad0e2b166574f16
Jan Nedbal <janedbal@gmail.com>
2025-04-04 14:18:36 +0200

    Fix mixed calls diagstics when custom excluder is used (#196)

--- tests/Rule/data/debug/exclude.php
+++ tests/Rule/data/debug/exclude.php
@@ -4,8 +4,8 @@ namespace DebugExclude;
 
 class Foo
 {
-    public static function mixedExcluder1() {} // error: Unused DebugExclude\Foo::mixedExcluder1 (all usages excluded by mixed excluder)
-    public static function mixedExcluder2() {} // error: Unused DebugExclude\Foo::mixedExcluder2 (all usages excluded by mixed excluder)
+    public static function mixedExcluder1() {} // error: Unused DebugExclude\Foo::mixedExcluder1 (all usages excluded by mixedPrefix excluder)
+    public static function mixedExcluder2() {} // error: Unused DebugExclude\Foo::mixedExcluder2 (all usages excluded by mixedPrefix excluder)
 }
 
 class Chld extends Foo {
--- tests/Rule/data/excluders/mixed/code.php
+++ tests/Rule/data/excluders/mixed/code.php
@@ -3,11 +3,11 @@
 namespace MixedExcluder;
 
 class SomeParent {
-    public function mixed1() {} // error: Unused MixedExcluder\SomeParent::mixed1 (all usages excluded by mixed excluder)
+    public function mixed1() {} // error: Unused MixedExcluder\SomeParent::mixed1 (all usages excluded by mixedPrefix excluder)
 }
 
 class Some extends SomeParent {
-    public function mixed2() {} // error: Unused MixedExcluder\Some::mixed2 (all usages excluded by mixed excluder)
+    public function mixed2() {} // error: Unused MixedExcluder\Some::mixed2 (all usages excluded by mixedPrefix excluder)
 }
 
 function test(Some $some) {
