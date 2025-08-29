018ddde851539074793a0f92ae279d0452d3919b
Jan Nedbal <janedbal@gmail.com>
2025-08-29 16:27:45 +0200

    TestsUsageExcluder: test composer autodetection (#250)

---
+++ tests/Excluder/TestsUsageExcluderTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace ShipMonk\PHPStan\DeadCode\Excluder;
+
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Testing\PHPStanTestCase;
+use ReflectionClass;
+use function realpath;
+
+class TestsUsageExcluderTest extends PHPStanTestCase
+{
+
+    public function testAutodetectComposerDevPaths(): void
+    {
+        $excluder = new TestsUsageExcluder(self::getContainer()->getByType(ReflectionProvider::class), true, null);
+
+        $excluderReflection = new ReflectionClass(TestsUsageExcluder::class);
+        $devPathsPropertyReflection = $excluderReflection->getProperty('devPaths');
+        $devPathsPropertyReflection->setAccessible(true);
+
+        self::assertSame([
+            realpath(__DIR__ . '/../../tests'),
+            realpath(__DIR__ . '/../../tests/Rule/data'),
+        ], $devPathsPropertyReflection->getValue($excluder));
+    }
+
+}
