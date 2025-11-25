98e5ca8cff945ca75440f4a0115163cee21813a5
Jan Nedbal <janedbal@gmail.com>
2025-11-25 09:36:03 +0100

    Test also fcc in attribute

--- tests/Rule/data/methods/first-class-callable.php
+++ tests/Rule/data/methods/first-class-callable.php
@@ -4,15 +4,14 @@ namespace DeadFirstClassCallable;
 
 class A {
 
+    #[MyAttribute(self::usedByFirstClassCallableInAttribute(...))] // since PHP 8.5
     public function used(): void
     {
         $callback = $this->usedByFirstClassCallable(...);
     }
 
-    public function usedByFirstClassCallable(): void
-    {
-
-    }
+    public function usedByFirstClassCallable(): void {}
+    public static function usedByFirstClassCallableInAttribute(): void {}
 }
 
 (new A())->used();
