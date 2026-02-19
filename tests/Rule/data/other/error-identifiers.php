7c683070cfad0d81ad4fbb17c7e33bb911c6dd01
Jan Nedbal <jan.nedbal@shipmonk.com>
2026-02-19 13:34:28 +0100

    Add neverWritten error identifier to error-identifiers test
    Demonstrates the shipmonk.deadProperty.neverWritten identifier
    alongside the existing neverRead, deadConstant, deadMethod and
    deadEnumCase identifiers.

--- tests/Rule/data/other/error-identifiers.php
+++ tests/Rule/data/other/error-identifiers.php
@@ -7,6 +7,8 @@ class Foo
 
     public int $count = 1; // @phpstan-ignore shipmonk.deadProperty.neverRead // error: Property ErrorIdentifiers\Foo::$count is never read
 
+    public int $writeCount; // @phpstan-ignore shipmonk.deadProperty.neverWritten // error: Property ErrorIdentifiers\Foo::$writeCount is never read // error: Property ErrorIdentifiers\Foo::$writeCount is never written
+
     public const BAR = 1; // @phpstan-ignore shipmonk.deadConstant
 
     public function method(): void {} // @phpstan-ignore shipmonk.deadMethod
